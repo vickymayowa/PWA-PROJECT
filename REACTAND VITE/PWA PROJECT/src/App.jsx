87b23f89908ea722fd9c30f0721cfc445a38f7fa
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-04-19 13:32:12 +0100

    first commit

---
+++ REACTAND VITE/PWA PROJECT/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card from './componnets/Card'
+
+function App() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then(registration => {
+            console.log('SW registered: ', registration);
+          })
+          .catch(registrationError => {
+            console.log('SW registration failed: ',
+              registrationError);
+        })
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <Card />
+    </>
+  )
+  }
+export default App
